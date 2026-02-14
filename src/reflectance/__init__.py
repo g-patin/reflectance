c980bbba05d50d02880aab9d9c38aae05316f052
g-patin <g.patin@monemail.com>
2026-02-14 22:23:01 +0100

    added and removed some functions

--- src/reflectance/__init__.py
+++ src/reflectance/__init__.py
@@ -1,3 +1,3 @@
-from .reflectance import RS, is_DB, get_datasets, get_creators, get_DB, get_DB_config, get_institutions, get_objects, get_persons, process_rawdata, create_DB, get_path_DB, update_DB_objects, update_DB_projects, add_new_institution, add_new_project, add_new_object, add_new_person, add_devices, add_references, get_devices, get_white_standards, set_colorimetry_info, get_colorimetry_info, set_devices_info, set_fibers_info, set_comments_info, set_devices_keys, set_config_info, set_DB
+from .reflectance import RS, is_DB, get_datasets, get_config, process_rawdata, reset_config, set_colorimetry_info, get_colorimetry_info, set_devices_info, set_filenaming_interim, set_filenaming_raw, set_comments_info, set_config_info, set_DB, remove_devices_info, set_institution_info, get_institution_info
 
 __version__ = "0.1.0"
