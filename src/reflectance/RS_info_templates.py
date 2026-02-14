5f9534cfc5a817dbab8cf84b0656d79e75643830
g-patin <g.patin@monemail.com>
2026-02-14 22:24:20 +0100

    changes in the devices and analysis dict

--- src/reflectance/RS_info_templates.py
+++ src/reflectance/RS_info_templates.py
@@ -1,30 +1,90 @@
+general_info = [
+    "measurement_type",
+    "authors",
+    "organization",
+    "datetime_analysis",
+    "datetime_processing",
+    "comment",
+]
 
-device_info = ["[DEVICE INFO]",
-        "device_ID",
-        "brand",
-        "model",                
-        "measurement_mode",
-        "zoom",
-        "iris",
-        "geometry",
-        "lamp",
-        "filter_ill",
-        "fiber_ill",
-        "fiber_coll",
-        "distance_ill_mm",
-        "distance_coll_mm",  
-        "specular_component",    
-        "white_standard",
+project_info = [
+    "[PROJECT INFO]",
+    "project_id",
+    "institution",
+    "start_date",
+    "end_date",
+    "project_leader",
+    "co_researchers",
+    "keywords",
+]
+
+object_info = [
+    "[OBJECT INFO]",
+    "object_id",
+    "object_category",
+    "object_technique",
+    "object_type",
+    "object_title",
+    "object_name",
+    "object_creator",
+    "object_date",
+    "object_owner",
+    "object_support",    
+]
+
+system_info = [
+    "[SYSTEM INFO]",
+    "system_id",
+    "system_name",
+    "geometry", 
+    "lamp",
+    "filter_ill",
+    "fiber_ill",
+    "fiber_coll",
+    "specular_component",
+    "system_params",
+]
+
+device_info = [
+    "[DEVICE INFO]",
+    "device_id",
+    "device_type",
+    "brand",
+    "model", 
+    "software_version",
+    "lamp",
+    "geometry",
+    "fiber_ill",
+    "fiber_coll", 
+    "filter",   
+    "specular_component",    
+    "device_params",            
 ]
 
 analysis_info = [
     "[ANALYSIS INFO]",
-        "meas_id",
-        "group",
-        "group_description",
-        "spot_size_mm",
-        "background",        
-        "integration_time_ms",
-        "average",        
-        "measurements_N",        
+    "meas_id",
+    "group",
+    "group_description",
+    "spot_size_mm",
+    "background",        
+    "integration_time_ms",
+    "average_scans", 
+    "smoothing_pixels",      
+    "measurements_N",  
+    "white_standard",      
+]
+
+colorimetric_info = [
+    "[COLORIMETRIC INFO]",
+    "illuminant",
+    "observer"
+]
+
+process_rawdata_functions = [
+    'RS_ASD',
+    'RS_Avt',
+    'RS_KM',
+    'RS_OO',
+    'RS_Tidas',     
 ]
