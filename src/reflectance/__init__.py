db148aca6be3ebc0b02444adc6bcc8ad1da0a7eb
g-patin <g.patin@monemail.com>
2026-08-21 16:20:32 +0200

    Create new docs with quarto in place of mkdocs

--- src/reflectance/__init__.py
+++ src/reflectance/__init__.py
@@ -1,3 +1,3 @@
-from .reflectance import RS, is_DB, get_datasets, get_config, process_rawdata, reset_config, set_colorimetry_info, get_colorimetry_info, set_devices_info, set_filenaming_interim, set_filenaming_raw, set_comments_info, set_config_info, set_DB, remove_devices_info, set_institution_info, get_institution_info
+from .reflectance import RS, is_DB, get_datasets, get_config, process_rawdata, reset_config, set_colorimetry_info, get_colorimetry_info, set_devices_info, set_filenaming_interim, set_filenaming_raw, set_comments_info, set_config_info, set_DB, remove_devices_info, set_institution_info, get_institution_info, set_systems_info, remove_systems_info, remove_institution_info, remove_comments_info
 
 __version__ = "0.2.0"
